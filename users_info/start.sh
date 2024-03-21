66843508c15cd5be237913cebb602b4cdefc09be
LuchnikKek <ilua.kabeshov01@mail.ru>
2024-03-22 01:22:52 +0300

    Исправлен `start.sh`. Больше не проверяет директорию на изменения.

--- users_info/start.sh
+++ users_info/start.sh
@@ -6,4 +6,4 @@ shopt -s nullglob
 #alembic upgrade head
 #exec скрипт для создания пользователей
 
-exec uvicorn src.main:app --host 0.0.0.0 --port "$API_PORT" --reload
+exec uvicorn src.main:app --host 0.0.0.0 --port "$API_PORT"
