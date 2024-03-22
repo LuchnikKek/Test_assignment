91a00d09dcf2efe30f9d1fcda103b6d2a819f801
LuchnikKek <ilua.kabeshov01@mail.ru>
2024-03-22 05:27:10 +0300

    Добавлен парсинг конфига с использованием `pydantic_settings`.

---
+++ users_info/src/core/config.py
@@ -0,0 +1,48 @@
+from functools import lru_cache
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ROOT_PATH: Path = Path(__file__).parent.parent.parent.parent
+ENV_FILEPATH: Path = ROOT_PATH / ".env"
+
+
+class PostgresSettings(BaseSettings):
+    """Конфигурация PostgreSQL."""
+
+    user: str
+    password: str
+    host: str
+    port: int
+    database: str
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILEPATH, env_file_encoding="utf-8", extra="ignore", env_prefix="USERS_PG_"
+    )
+
+    @property
+    def dsn(self):
+        """Строка, содержащая DSN для подключения."""
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+
+
+class ApiSettings(BaseSettings):
+    """Конфигурация API."""
+
+    port: int
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILEPATH, env_file_encoding="utf-8", extra="ignore", env_prefix="USERS_API_"
+    )
+
+
+class Settings(BaseSettings):
+    postgres: PostgresSettings = PostgresSettings()
+    api: ApiSettings = ApiSettings()
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+@lru_cache
+def get_settings():
+    return Settings()
